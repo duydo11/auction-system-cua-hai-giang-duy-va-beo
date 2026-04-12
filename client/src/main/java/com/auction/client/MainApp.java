f967d9816a432a532c233f8d0f891b20788c232b
25023225-HaGiang <25023225@vnu.edu.vn>
2026-04-13 00:29:14 +0700

    Sửa lại giao diện Login và vẽ thêm Register

--- client/src/main/java/com/auction/client/MainApp.java
+++ client/src/main/java/com/auction/client/MainApp.java
@@ -1,4 +1,37 @@
 package com.auction.client;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Montserrat-Bold.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Montserrat-Regular.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/PlaywriteIE-Regular.ttf"), 14);
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Login.fxml"));
+            Parent root = loader.load();
+
+            primaryStage.setTitle("Hệ thống đấu giá - Đăng nhập");
+            primaryStage.setScene(new Scene(root));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.err.println("Lỗi: Không tìm thấy file Login.fxml hoặc file FXML có lỗi cấu trúc.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
