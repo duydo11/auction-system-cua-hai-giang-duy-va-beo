5a585da29050cae40bd761f3d1d494d8308fd620
HoangDuong <25023252@vnu.edu.vn>
2026-05-03 08:52:18 +0700

    chore: entry point ServerMain và Launcher client

--- server/src/main/java/com/auction/server/ServerMain.java
+++ server/src/main/java/com/auction/server/ServerMain.java
@@ -1,4 +1,11 @@
 package com.auction.server;
 
+import com.auction.server.network.SocketServer;
+
 public class ServerMain {
+    public static void main(String[] args) {
+        System.out.println("🚀 Starting Auction Server...");
+        SocketServer server = new SocketServer();
+        server.start();
+    }
 }
